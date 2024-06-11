ed24a818cea00515d17bfba8485f420eada2ba99
Ivan Loyola <ivan@iloyola.com>
2024-06-11 15:17:03 -0400

    Move header component to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from 'vue'
 
+// components
+import Header from "./components/Header.vue"
+
 const AsyncHome = defineAsyncComponent(() =>
     import('./components/AsyncHome.vue')
 )
+
+
 </script>
 
 <template>
+    <Header />
     <Suspense>
         <template #default>
             <AsyncHome />
