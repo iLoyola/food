4aafb75308d603be0c0c5b2bb56f6ba8d892fc3b
Ivan Loyola <ivan@iloyola.com>
2024-06-11 16:36:06 -0400

    Shopping page "Coming Soon"

--- src/pages/Shopping.vue
+++ src/pages/Shopping.vue
@@ -1,1 +1,8 @@
-<template></template>
+<template>
+    <div class="heading w-full border-black border-b border-solid mb-1">
+        <h1 class="text-3xl font-bold text-black m-4">Shopping</h1>
+    </div>
+    <div>
+        <h2>Coming Soon</h2>
+    </div>
+</template>
